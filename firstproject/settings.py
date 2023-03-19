d7bc656d78221d7c5b06b59592c894c7aae50700
yama0721 <70010039+yama0721@users.noreply.github.com>
2023-03-19 14:14:31 +0900

    Update settings.py
    ALLOWED_HOSTSに仮想マシンを追加

--- firstproject/settings.py
+++ firstproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#6)6)+*#(fbcdb=(ebqkv+15uxfuv=sy$g%479go38(ve$ali_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com','4.216.185.119']
 
 
 # Application definition
